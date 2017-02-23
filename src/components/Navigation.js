72511e700307cb1ae856ce6ad5da8238745810a8
Melvin Zehl <mhzehl@gmail.com>
2017-02-23 15:40:23 +0100

    Add navigation component

---
+++ src/components/Navigation.js
@@ -0,0 +1,47 @@
+import React, { PureComponent, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { history } from '../store'
+import signOut from '../actions/user/sign-out'
+import AppBar from 'material-ui/AppBar'
+import IconButton from 'material-ui/IconButton'
+import Group from 'material-ui/svg-icons/social/group'
+import FlatButton from 'material-ui/FlatButton'
+
+export class Navigation extends PureComponent {
+  static propTypes = {
+    signedIn: PropTypes.bool.isRequired,
+  }
+
+  signOut(event) {
+    event.preventDefault()
+    this.props.signOut()
+  }
+
+  signUp() {
+    history.push('/sign-up')
+  }
+
+  goHome() {
+    history.push('/')
+  }
+
+  render() {
+    const { signedIn } = this.props
+    return (
+      <AppBar
+        title="CodaiMatcher"
+        iconElementLeft={<IconButton onClick={this.goHome}><Group /></IconButton>}
+        iconElementRight={signedIn ?
+          <FlatButton label="Sign out" onClick={this.signOut.bind(this)} /> :
+          <FlatButton label="Sign up" onClick={this.signUp} />
+        }
+      />
+    )
+  }
+}
+
+const mapStateToProps = ({ currentUser }) => ({
+  signedIn: (!!currentUser && !!currentUser._id)
+})
+
+export default connect(mapStateToProps, { signOut })(Navigation)
