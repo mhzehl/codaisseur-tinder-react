ac705a1246dab306832eec223ccf479019e6468e
Melvin Zehl <mhzehl@gmail.com>
2017-02-23 15:42:24 +0100

    Change App component, render new comoponents, change routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React from 'react'
-import { Provider } from 'react-redux'
 import store from './store'
 import Title from './components/Title'
+import Navigation from './components/Navigation'
+import Match from './components/Match'
 import './App.sass'
 
+// Material UI Components
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import muiTheme from './assets/styles/theme'
+
 class App extends React.Component {
+  static childContextTypes = {
+    muiTheme: React.PropTypes.object.isRequired,
+  }
+
+  getChildContext() {
+    return { muiTheme }
+  }
+
+
   render() {
     return (
-      <div className="app">
-        { this.props.children }
-      </div>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div className="app">
+          <Navigation />
+          { this.props.children }
+          <Match />
+        </div>
+      </MuiThemeProvider>
+
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,19 +4,20 @@ import { Provider } from 'react-redux'
 import { Router, Route, IndexRoute } from 'react-router'
 import store, { history } from './store'
 import injectTapEventPlugin from 'react-tap-event-plugin'
-import App from './App'
-import SignUp from './users/SignUp'
-import SignIn from './users/SignIn'
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin()
 
+import App from './App'
+import SignUp from './users/SignUp'
+import SignIn from './users/SignIn'
 
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
       <Route path="/" component={App}>
+        <IndexRoute />
         <Route path="/sign-up" component={SignUp} />
         <Route path="/sign-in" component={SignIn} />
       </Route>
