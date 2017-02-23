f8c7c3ef9a26dbb03f17d259126a08e9c877380d
Melvin Zehl <mhzehl@gmail.com>
2017-02-23 15:39:54 +0100

    Create Match component

---
+++ src/components/Match.js
@@ -0,0 +1,18 @@
+import React, { PureComponent } from 'react'
+import Student from './Student'
+import MatchIcon from '../assets/images/match-icon.png'
+import './Match.sass'
+
+class Match extends PureComponent {
+  render() {
+    return(
+      <div className='match'>
+        <Student />
+        <img src={ MatchIcon } className='match-icon' />
+        <Student name="Johan Ja" />
+      </div>
+    )
+  }
+}
+
+export default Match
